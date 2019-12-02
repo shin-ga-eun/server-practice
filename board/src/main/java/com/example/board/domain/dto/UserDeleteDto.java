a69ea24cc6d2b08aa2774cfb10fbf4fda03d1434
신가은 <sge7102@naver.com>
2019-12-03 02:18:57 +0900

    user crud 완료. board create 완료.

---
+++ board/src/main/java/com/example/board/domain/dto/UserDeleteDto.java
@@ -0,0 +1,18 @@
+package com.example.board.domain.dto;
+
+public class UserDeleteDto {
+	
+	private String identity; //아이디
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+
+	
+	
+	
+}
